02caa03430b6809a01c43d7800092a4183ca7ab6
yerepf <yeremyyaelpujolsfelix@gmail.com>
2024-04-23 21:33:24 -0400

    components was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { NavbarComponent } from "./navbar/navbar.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AboutComponent, EducationComponent, NavbarComponent]
 })
 export class AppComponent {
-  title = 'MyApp';
+    title='a';
 }
